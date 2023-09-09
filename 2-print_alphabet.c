d9debf3253dca06fe8d3d6324e07079a973197ec
SthablyM <mathapelofentse@icloud.com>
2023-09-09 07:57:11 -0700

    2-print_alphabet.c

---
+++ 2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - The entry point
+ *
+ *Return: (0)
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+
+}
