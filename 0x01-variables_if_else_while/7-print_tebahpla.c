d8b056bc0a22813105c313900d0d69b12105177e
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-11 06:51:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'z' ; k <= 'a' ; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
