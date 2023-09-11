73400ae21c160372ca4e712a95a6dcc5b9590d93
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-11 06:10:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+		printf("%d", k);
+	printf("\n");
+	return (0);
+}
