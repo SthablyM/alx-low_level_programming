0927199159929d4a95b47f9b751ee41a11c5f1af
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-11 07:57:42 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  prints all possible combinations of single-digit numbers
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		putchar(k + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
