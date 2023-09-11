eb8051e3c2ca2e8951d191471561d4688fd28b3e
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-11 06:33:21 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10 ; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
