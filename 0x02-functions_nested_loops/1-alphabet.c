7d8c908c8e84766a1c732f909a14fbbc295485af
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-12 08:59:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints the alphabet
+ *
+ * Return: Always (0)
+ */
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
