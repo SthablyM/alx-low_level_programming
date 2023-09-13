9b49d4ecf628dc27d5e085bfb373d5e2750af8c2
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-13 08:20:52 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - Add two values
+ *@a: first parameter to be checked
+ *@b: second parameter to be checked
+ *Return: value
+ */
+int add(int a, int b)
+{
+
+	return (a + b);
+}
