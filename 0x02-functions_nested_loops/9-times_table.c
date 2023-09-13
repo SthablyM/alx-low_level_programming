9a50888512ba2b6955bf2a5b17b31534b6a83b35
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-13 07:46:38 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *times_table - prints 9 times table
+ *
+ *Return:void
+ */
+void times_table(void)
+{
+	int k, l, result;
+
+	for (k = 0 ; k < 10 ; k++)
+	{
+		for (l  = 0 ; l < 10 ; l++)
+		{
+			result = k * l;
+			if (l == 0)
+				printf("%d, ", result);
+			else
+			{
+				printf("%2d", result);
+				if (l != 0)
+					printf(",");
+			}
+		}
+		printf("\n");
+	}
+}
