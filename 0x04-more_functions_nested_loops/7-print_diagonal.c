3a7a11c0f7817d6e3396068a573ad8eb964fa01d
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-15 13:52:24 -0700

    another attempt to diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,12 +13,11 @@ void print_diagonal(int n)
 		int i, j;
 
 		for (i = 0 ; i < n ; i++)
-		{	
-			for (j = 1; j <= i; j++){
+		{
+			for (j = 1; j <= i; j++)
 				_putchar(' ');
-			}			
 			_putchar('\\');
 			_putchar('\n');
-		}		
+		}
 	}
 }
