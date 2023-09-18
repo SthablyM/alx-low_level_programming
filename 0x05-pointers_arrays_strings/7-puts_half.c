2ab91435dd03993d1b7bd6ec7fb5803900ff633a
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-18 14:25:28 -0700

    added puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *puts_half - function that print half of a string
+ *@str: parameter to be checked
+ *Return: void
+ */
+void puts_half(char *str)
+{
+	int count = 0, i, k;
+
+	for (k = 0 ; str[k] != '\0' ; k++)
+		count++;
+
+	i = (count / 2);
+	if (count % 2 != 0)
+	{
+		i = (count - 1) / 2;
+	}
+
+	while (i < count)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+
+}
