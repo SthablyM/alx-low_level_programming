835a12d102f5c8e286d989f0addd052cc425a7df
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-18 11:01:57 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_puts -  function that prints a string
+ *@str: paramete to be checked
+ *Return: void
+ */
+void _puts(char *str)
+{
+	int k;
+
+	for (k = 0 ; str[k] != '\0'; k++)
+		_putchar(str[k]);
+}
