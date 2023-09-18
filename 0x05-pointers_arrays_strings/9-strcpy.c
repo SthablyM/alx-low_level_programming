b1059d4f8a3668c339468bcddd667bac0f8992ad
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-18 15:23:03 -0700

    adding strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
- *print_array - function that print array elements
+ *_strcpy - function copies the string pointed to by src 
  *@dest: A pointer to destination of string
  *@src: A pointer to source string to copy from
- *Return: void
+ *Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
+	int index = 0;
 
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
 }
