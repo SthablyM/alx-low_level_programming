0a66526767d9471d51206775b32847ee3399c965
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-18 15:35:16 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,16 @@
 #include "main.h"
 /**
- *_strcpy - function copies the string pointed to by src 
+ *_strcpy - function copies the string pointed to by src
  *@dest: A pointer to destination of string
  *@src: A pointer to source string to copy from
  *Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int k;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
+	for (k = 0 ; src[k] != '\0' ; k++)
+		dest[k] = src[k];
+	dest[k] = '\0';
 	return (dest);
 }
