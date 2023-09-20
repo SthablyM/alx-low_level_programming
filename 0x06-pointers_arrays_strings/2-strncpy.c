e5ec204051b0f1d6b9867ae545f1c84a92b42b2f
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-20 13:52:28 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ **_strncpy - main function
+ *@dest: parameter to be checked
+ *@src: parameter to be checked
+ *@n: parameter to checked
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *s = dest;
+
+	while (*src != '\0' && n > 0)
+	{	*s++ = *src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*s++ = '\0';
+		n--;
+	}
+	return (dest);
+}
