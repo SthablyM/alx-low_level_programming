61419b78586a0fe091160c3b8f04534c7d5d4538
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-20 21:11:14 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (*s1 != *s2)
-		return (*s1 - *s2);
-	}
-	{
-		s1++;
-		s2++;
+		{
+			return (*s1 - *s2);
+		}
+
+	s1++;
+	s2++;
 	}
 	return (*s1 - *s2);
 }
