0c231cc8b7b75cdd1594f44affd11bcf647a2dc2
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-22 05:21:50 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ **_strchr -  function that locates a character in a string
+ *@s: parameter to be checked
+ *@c: paramete to be checked
+ *Return: c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
