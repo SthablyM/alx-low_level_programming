e45e7dc4910b866074654039a305dc31a7470021
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-22 15:10:55 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,11 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i <= (size * size); i = i +size + 1)
+	for (i = 0; i <= (size * size); i = i + size + 1)
 	{
 		sum1 = sum1 + a[i];
 	}
-	for (r = size -1; r <= (size * size)-size; r = r + size - 1)
+	for (r = size - 1; r <= (size * size) - size; r = r + size - 1)
 	{
 		sum2 = sum2 + a[r];
 	}
