20b760ef2ab67a2a9878fce4bb13aef626aa9af8
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-22 05:28:04 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
  **_strchr -  function that locates a character in a string
  *@s: parameter to be checked
  *@c: paramete to be checked
- *Return: c or NULL
+ *Return: s or NULL
  */
 char *_strchr(char *s, char c)
 {
