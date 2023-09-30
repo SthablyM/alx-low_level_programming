9cfdc83254121af27d78eccdeaa22e2c1b04a0bf
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-09-30 03:09:43 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function that prints a name 
+ * @agrc: number of commands line
+ * @argv: arrays that contain commands line
+ * Return: 0 when successful
+ */
+int main(int agrc __attribute__((unused)), char *argv[])
+{
+	printf("%s \n", argv[0]);
+		return (0);
+}
