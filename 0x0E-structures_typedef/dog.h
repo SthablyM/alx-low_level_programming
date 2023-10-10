35cb3b5aa46d4846635558556bb5546eba02a889
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-10-10 09:12:27 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - dog's information
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: dog's owner
+ *
+ *Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ *dog_t - tiypedef for struct dog
+ */
+typedef struct dog dog_t;
+#endif
