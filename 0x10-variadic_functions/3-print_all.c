b634af1a2e1fd6e164e5f8ac4f228f74380a3953
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-10-12 17:14:25 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 				case 's':
 					str = va_arg(args, char *);
 					if (!str)
-						str = ("(nill)");
+						str = ("(nil)");
 					printf("%s%s", sep, str);
 					break;
 				default:
