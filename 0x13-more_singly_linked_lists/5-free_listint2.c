ed52cd932131e526ba5748e8dfa3b13824b3b206
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-10-24 01:48:44 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
 	listint_t *next;
-	
+
 	if (head == NULL)
 		return;
 
@@ -19,7 +19,7 @@ void free_listint2(listint_t **head)
 		next = current;
 		current = current->next;
 		free(next);
-	}	
+	}
 	*head = NULL;
 
 }
