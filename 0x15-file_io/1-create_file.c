007ddbe86aa1f4c013d72187064c9e59c5c99c85
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-10-30 10:04:10 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,38 @@
 #include "main.h"
 /**
  *create_file - function that creates a file
- *@filename: to be checked
- *@text_content: to be checked
+ *@filename: filename
+ *@text_content: content
  *Return: -1 if filename is NULL
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file_dsp;
 	int l;
-	ssize_t bytes_written;
+	int bytes_written;
 
 	if (!filename)
-	{
+
 		return (-1);
-	}
-	file_dsp = open(filename, O_WRONLY | O_APPEND);
+	
+	file_dsp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (file_dsp == -1)
-	{
+
 		return (-1);
-	}
-	if (text_content)
-	{
-		for (l = 0; text_content[l]; l++);
-		bytes_written = write(file_dsp, text_content,
-				strlen(text_content));
-		if (bytes_written == -1)
-		{
-			return (-1);
-		}
-	}
+
+	if (!text_content)
+		text_content = "";
+	
+	for (l = 0; text_content[l]; l++);
+	
+	bytes_written = write(file_dsp, text_content, l);
+	
+	if (bytes_written == -1)
+		
+		return (-1);
+	
 	close(file_dsp);
+	
 	return (1);
 }
