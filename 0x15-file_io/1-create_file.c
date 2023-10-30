6c824ea18e41035685ba2d42f925faf96c0d140c
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-10-30 10:36:31 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,6 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 
 		return (-1);
-	
 	file_dsp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_dsp == -1)
@@ -23,16 +22,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		text_content = "";
-	
-	for (l = 0; text_content[l]; l++);
-	
-	bytes_written = write(file_dsp, text_content, l);
-	
+	for (l = 0; text_content[l]; l++)
+		bytes_written = write(file_dsp, text_content, l);
 	if (bytes_written == -1)
-		
 		return (-1);
-	
 	close(file_dsp);
-	
 	return (1);
 }
