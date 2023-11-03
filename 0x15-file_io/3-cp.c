97a51ba82f05f2713037e0bb1d53f7916b9736f3
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-11-03 07:36:33 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	bytes_read  = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	do {
+	{
 		if (file_from == -1 || bytes_read == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: can't read from file%s\n",
