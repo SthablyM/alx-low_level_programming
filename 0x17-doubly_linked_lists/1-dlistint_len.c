e935e25fd3ada17c8924aef73130b5aec7084520
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-12-07 09:14:42 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,12 +6,18 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t node_count = 0;
+	int node_count;
 
+	node_count = 0;
+
+	if (h == NULL)
+		return (node_count);
+	while (h->prev != NULL)
+		h = h->prev;
 	while (h != NULL)
 	{
-		h = h->next;
 		node_count++;
+		h = h->next;
 	}
-	return (node_count)
+	return (node_count);
 }
