d0ed8a1e53a0a7ed6fda610d9da11c332f37228d
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-12-07 10:18:10 -0800

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *free_dlistint - function that frees a dlistint_t
+ *@head: pointer
+ *Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	dlistint_t *next_node;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
