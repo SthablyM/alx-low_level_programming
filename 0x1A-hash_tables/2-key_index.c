35258f3a29bcdea23952309f7d2a2f12d834f406
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-12-22 03:11:04 -0800

    2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ *key_index - function that gives you the index of a key
+ *@key: the key to get  the index
+ *@size: size of array
+ *Return: index of the key
+ *Description: uses the djb2 algorithm.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
