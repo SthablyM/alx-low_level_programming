60c16e35fbd3a2cb310a52b1e1cc3ae757e0f508
SthablyM <mathapelofentse@icloud.com>
2024-02-16 05:55:33 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
 	ht = calloc(1, sizeof(hash_table_t));
 	if (ht == NULL)
-	{
 		return (NULL);
-	}
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
