b95495bc4a0c6a9ef9a4c8458c92aadcde08314c
SthablyM <mathapelofentse@icloud.com>
2024-04-05 07:37:44 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,8 +13,8 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 c += 1
-                if (j > 0 and grid[i][j -1] == 1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     e += 1
-                if (i > 0 and grid[i -1][j] ==1):
+                if (i > 0 and grid[i - 1][j] == 1):
                     e += 1
     return c * 4 - e * 2
